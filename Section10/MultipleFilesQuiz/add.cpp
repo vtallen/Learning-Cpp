131ff279b7ddbc245b7f8627b4d98f29a8124590
vtallen <vtallen@protonmail.com>
2023-01-31 17:18:02 -0500

    Completed the learncpp.com multiple files lesson

---
+++ Section10/MultipleFilesQuiz/add.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Vincent Allen on 1/31/23.
+//
+
+int add(int x, int y) {
+    return x + y;
+}
---
+++ Section10/MultipleFilesQuiz/getInt.cpp
@@ -0,0 +1,11 @@
+//
+// Created by Vincent Allen on 1/31/23.
+//
+#include <iostream>
+
+int getInteger() {
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+    return x;
+}
