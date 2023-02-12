dcde59e63c924a1de2f2e34c44627b135685b92c
vtallen <vtallen@protonmail.com>
2023-02-12 18:13:40 -0500

    Completed 6

---
+++ Section 6/Question2/constants.h
@@ -0,0 +1,10 @@
+//
+// Created by Vincent Allen on 2/12/23.
+//
+
+#ifndef SECTION_6_CONSTANTS_H
+#define SECTION_6_CONSTANTS_H
+namespace constants {
+    inline constexpr int max_class_size {35};
+}
+#endif //SECTION_6_CONSTANTS_H
---
+++ Section 6/Question3/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+bool passOrFail() {
+    static int numRun {0};
+
+    ++numRun;
+    if (numRun <= 3) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main()
+{
+    std::cout << "User #1: " << (passOrFail() ? "Pass\n" : "Fail\n");
+    std::cout << "User #2: " << (passOrFail() ? "Pass\n" : "Fail\n");
+    std::cout << "User #3: " << (passOrFail() ? "Pass\n" : "Fail\n");
+    std::cout << "User #4: " << (passOrFail() ? "Pass\n" : "Fail\n");
+    std::cout << "User #5: " << (passOrFail() ? "Pass\n" : "Fail\n");
+
+    return 0;
+}
